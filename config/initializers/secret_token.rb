0e3b1d00a68eb5ab208cc66c2a1e4463b55002ac
nurhayati100 <haya_nur1995@yahoo.com>
2014-02-17 22:03:07 +0700

    add new application in github

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+App::Application.config.secret_key_base = 'df5c2bb07367f93748ad38ce6ed32cd5627cc0a95c3f9452e0021faa673d6095ae82c533997b22e67d93adca4239416bbb1f7a55c1d5871266814ff32c4b728a'
